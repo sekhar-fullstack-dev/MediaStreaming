1f988b8cf1da9fec08b1f936831e62a746599fc4
sekhar-fullstack-dev <140637559+sekhar-fullstack-dev@users.noreply.github.com>
2024-08-06 08:32:46 +0530

    aug

--- app/src/main/java/com/example/mediastreaming/utils/Constants.java
+++ app/src/main/java/com/example/mediastreaming/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.example.mediastreaming.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://ec2-65-0-3-226.ap-south-1.compute.amazonaws.com:80";
-    public static final String RTMP_STREAM_IP = "ec2-65-0-3-226.ap-south-1.compute.amazonaws.com:1935";
+    public static final String BASE_URL = "http://ec2-13-200-254-228.ap-south-1.compute.amazonaws.com:80";
+    public static final String RTMP_STREAM_IP = "ec2-13-200-254-228.ap-south-1.compute.amazonaws.com:1935";
 }
